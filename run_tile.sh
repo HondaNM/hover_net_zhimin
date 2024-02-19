e1bca9ba89b113863cd55c48a26d7a1034926757
HondaNM <52192141+HondaNM@users.noreply.github.com>
2024-02-19 15:57:55 -0600

    test_run_tile

--- run_tile.sh
+++ run_tile.sh
@@ -4,12 +4,12 @@ python run_infer.py \
 --type_info_path=type_info.json \
 --batch_size=64 \
 --model_mode=fast \
---model_path=shared/anastasio-s2/SI/TCVAE/DL_feature_interpretation/pretrained/hovernet_original_consep_notype_tf2pytorch.tar\
+--model_path='/shared/anastasio-s2/SI/TCVAE/DL_feature_interpretation/pretrained/hovernet_original_consep_notype_tf2pytorch.tar'\
 --nr_inference_workers=8 \
 --nr_post_proc_workers=16 \
 tile \
---input_dir=shared/anastasio-s2/SI/TCVAE/DL_feature_interpretation/dataset_with_padding/train\
---output_dir=shared/anastasio-s2/SI/TCVAE/DL_feature_interpretation/result \
+--input_dir='/shared/anastasio-s2/SI/TCVAE/DL_feature_interpretation/dataset_with_padding/train'\
+--output_dir='/shared/anastasio-s2/SI/TCVAE/DL_feature_interpretation/result' \
 --mem_usage=0.1 \
 --draw_dot \
 --save_qupath
