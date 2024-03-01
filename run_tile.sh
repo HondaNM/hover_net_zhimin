01773b9a4ce3527db65ae84c4de338947904ab3e
Zhimin Wang <zhiminw2@anastasio3.bioen.illinois.edu>
2024-03-01 11:04:52 -0600

    modified

--- run_tile.sh
+++ run_tile.sh
@@ -1,15 +1,16 @@
+#!/bin/bash
 python run_infer.py \
---gpu='1' \
---nr_types=6 \
+--gpu='4' \
+--nr_types=0 \
 --type_info_path=type_info.json \
---batch_size=64 \
+--batch_size=32 \
 --model_mode=fast \
---model_path='/shared/anastasio-s2/SI/TCVAE/DL_feature_interpretation/pretrained/hovernet_original_consep_notype_tf2pytorch.tar'\
+--model_path=/shared/anastasio-s2/SI/TCVAE/DL_feature_interpretation/pretrained/hovernet_original_consep_notype_tf2pytorch.tar \
 --nr_inference_workers=8 \
---nr_post_proc_workers=16 \
+--nr_post_proc_workers=8 \
 tile \
---input_dir='/shared/anastasio-s2/SI/TCVAE/DL_feature_interpretation/dataset_with_padding/train'\
---output_dir='/shared/anastasio-s2/SI/TCVAE/DL_feature_interpretation/result' \
---mem_usage=0.1 \
+--input_dir=/shared/anastasio-s2/SI/TCVAE/DL_feature_interpretation/dataset_with_padding/train \
+--output_dir=/shared/anastasio-s2/SI/TCVAE/DL_feature_interpretation/result \
+--mem_usage=0.2 \
 --draw_dot \
---save_qupath
+#--save_qupath
