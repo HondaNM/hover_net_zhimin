27abf78af85901dec679b573cfe3c58056707c9f
Zhimin Wang <zhiminw2@compton.bioen.illinois.edu>
2024-03-12 18:52:50 -0500

    nohup

--- run_tile.sh
+++ run_tile.sh
@@ -4,7 +4,7 @@ python run_infer.py \
 --nr_types=0 \
 --type_info_path=type_info.json \
 --batch_size=32 \
---model_mode=fast \
+--model_mode=original \
 --model_path=/shared/anastasio-s2/SI/TCVAE/DL_feature_interpretation/pretrained/hovernet_original_consep_notype_tf2pytorch.tar \
 --nr_inference_workers=8 \
 --nr_post_proc_workers=8 \
