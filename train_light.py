4655e08d1dddf9edd37e3b4852cc4d9addace59c
HondaNM <52192141+HondaNM@users.noreply.github.com>
2024-05-06 16:27:38 -0500

    light

--- train_light.py
+++ train_light.py
@@ -7,12 +7,12 @@ import seaborn as sns
 import joblib
 
 # Load dataset
-data_path = 'filtered_features.csv'
+data_path = 'updated_mean_features.csv'
 data = pd.read_csv(data_path)
 
 # Prepare the dataset
-X = data.drop('Extracted_Label', axis=1)
-y = data['Extracted_Label']
+X = data.drop(['Label','image_file'], axis=1)
+y = data['Label']
 
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -29,7 +29,7 @@ num_round = 100
 bst = lgb.train(params, train_data, num_round, valid_sets=[test_data], early_stopping_rounds=10)
 
 # Save the model
-model_path = '/shared/anastasio-s2/SI/TCVAE/DL_feature_interpretation/result/lightGBM/lightgbm_model.pkl'
+model_path = '/shared/anastasio-s2/SI/TCVAE/DL_feature_interpretation/result/lightGBM/lightgbm_model_mean.pkl'
 joblib.dump(bst, model_path)
 
 # Alternatively, for a custom plot and to get feature importance in a DataFrame:
@@ -41,14 +41,14 @@ feature_importance_df = feature_importance_df.sort_values('Importance', ascendin
 # Print feature importances
 print(feature_importance_df)
 # Assuming 'feature_importances' is your DataFrame
-feature_importance_df.to_csv('/shared/anastasio-s2/SI/TCVAE/DL_feature_interpretation/result/lightGBM/feature_importance_scores.csv', index=False)
+feature_importance_df.to_csv('/shared/anastasio-s2/SI/TCVAE/DL_feature_interpretation/result/lightGBM/feature_importance_scores_mean.csv', index=False)
 
 # Plotting the top 20 features
 plt.figure(figsize=(30, 15))
 sns.barplot(x="Importance", y="Feature", data=feature_importance_df.head(20))
 plt.title("Top 20 Feature Importances")
 plt.tight_layout()
-plt.savefig('/shared/anastasio-s2/SI/TCVAE/DL_feature_interpretation/result/lightGBM/top20_feature_importance_lightgbm.png', dpi=300)  # Save the figure
+plt.savefig('/shared/anastasio-s2/SI/TCVAE/DL_feature_interpretation/result/lightGBM/top20_feature_importance_lightgbm_mean.png', dpi=300)  # Save the figure
 plt.show()
 
 # Print accuracy for reference
